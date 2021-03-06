179f3dac989036b06aff009bf01412a5f9bf3650
Anas AbuZaitoun <anasomarzaitoun@gmail.com>
2021-03-06 18:12:24 +0200

    Added release

--- app/src/main/java/com/abuzaitoun/verificationcodeapp/MainActivity.java
+++ app/src/main/java/com/abuzaitoun/verificationcodeapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.abuzaitoun.verificationcodeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
